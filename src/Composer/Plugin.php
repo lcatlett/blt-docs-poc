f1bcbbae002d4a62b7dabc15d3fe41f0e57a9675
Dane Powell <git@danepowell.com>
2019-03-27 15:34:32 -0700

    Slimmed down.

--- src/Composer/Plugin.php
+++ src/Composer/Plugin.php
@@ -2,43 +2,14 @@
 
 namespace Acquia\Blt\Composer;
 
-use Acquia\Blt\Robo\Common\ArrayManipulator;
-use Acquia\Blt\Update\Updater;
 use Composer\Composer;
-use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
-/**
- *
- */
-class Plugin implements PluginInterface, EventSubscriberInterface {
+class Plugin implements PluginInterface {
 
-  /**
-   * @var Composer
-   */
-  protected $composer;
-  /**
-   * @var IOInterface
-   */
-  protected $io;
 
-  /**
-   * Apply plugin modifications to composer.
-   *
-   * @param Composer $composer
-   * @param IOInterface $io
-   */
   public function activate(Composer $composer, IOInterface $io) {
-
   }
-
-  /**
-   * Returns an array of event names this subscriber wants to listen to.
-   */
-  public static function getSubscribedEvents() {
-    return array();
-  }
-
 
 }
